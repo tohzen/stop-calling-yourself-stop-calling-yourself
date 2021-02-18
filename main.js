f3e45bf46d9f00c496531a42dc16856168d6fabf
Tohzen <melvin.l.mobley@gmail.com>
2021-02-18 15:41:15 -0500

    finished

---
+++ main.js
@@ -0,0 +1,33 @@
+function whodat (str) {
+    output = str + "???"
+    return console.log(output); 
+}
+whodat("goose")
+
+function spacemeharder (str1, str2) {
+    output = str1 + " " + str2
+    return console.log(output); 
+}
+spacemeharder(10,2)
+
+function take3 (num1, num2, num3) {
+    output = num1 - num2 -num3
+    return console.log(output); 
+}
+take3(15,5,5)
+
+
+function triplehomerun (str) {
+    output = str + str + str
+    return console.log(output); 
+}
+triplehomerun("dog")
+
+function circleroots (num1) {
+    output = Math.sqrt(num1)
+    return console.log(output); 
+}
+circleroots(191)
+
+
+
